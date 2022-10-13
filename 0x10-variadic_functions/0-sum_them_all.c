b6db2ba43b1abb0416910b140480d797cd976f94
Niyi07 <emaniyi64@gmail.com>
2022-10-13 12:36:12 +0100

    alx-low-level-language

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,20 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-	unsigned int i;
-	va_list arguments;
+	unsigned int i = 0;
+	va_list args;
 
-	if (n)
+	if (n != 0)
 	{
-		va_start(arguments, n);
-		for (i = 0; i < n; i++)
+		va_start(args, n);
+		while (i < n)
 		{
-			sum += va_arg(arguments, int);
+			sum += va_arg(args, int);
+
+			1++;
 		}
-		va_end(arguments);
+		va_end(args);
+		return(sum);
 	}
-	return (sum);
+	return (0);
 }
